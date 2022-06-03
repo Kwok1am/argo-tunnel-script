b30328d76578ad2736a22580e262886869d4edac
yuuki410 <yuuki410@163.com>
2022-06-03 15:20:09 +0800

    remove login tips

--- argo.sh
+++ argo.sh
@@ -55,7 +55,7 @@ installCloudFlared() {
 	wget -N https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-$cpuArch
 	mv cloudflared-linux-$cpuArch /usr/bin/cloudflared
 	chmod +x /usr/bin/cloudflared
-	[[ $loginStatus=="Unlogined" ]] && green "Please open the fllow link in your boweser and login your account:" && cloudflared tunnel login
+	[[ $loginStatus=="Unlogined" ]] && cloudflared tunnel login
 	back2menu
 }
 
