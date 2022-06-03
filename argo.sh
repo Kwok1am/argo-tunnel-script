b0f301b004cfe34991ba715088ddf47670f39861
yuuki410 <yuuki410@163.com>
2022-06-03 15:11:05 +0800

    fix

--- argo.sh
+++ argo.sh
@@ -54,10 +54,7 @@ checkStatus() {
 installCloudFlared() {
 	wget -N https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-$cpuArch
 	mv cloudflared-linux-$cpuArch /usr/bin/cloudflared
-	if [[ $loginStatus=="Unlogined" ]] ; then
-	    green "Please open the fllow link in your boweser and login your account:"
-	    cloudflared tunnel login
-	fi
+	[[ $loginStatus=="Unlogined" ]] && green "Please open the fllow link in your boweser and login your account:" && cloudflared tunnel login
 	back2menu
 }
 
